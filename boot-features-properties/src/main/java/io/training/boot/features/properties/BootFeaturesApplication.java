e0dbb24871c93bf52f8622b8ff1a16072931400b
Percy0601 <2008dreams@2008.sina.com>
2016-08-26 14:47:31 +0800

    初始化项目

---
+++ boot-features-properties/src/main/java/io/training/boot/features/properties/BootFeaturesApplication.java
@@ -0,0 +1,12 @@
+package io.training.boot.features.properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootFeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootFeaturesApplication.class, args);
+	}
+}
---
+++ boot-features-properties/src/test/java/io/training/boot/features/properties/BootFeaturesApplicationTests.java
@@ -0,0 +1,16 @@
+package io.training.boot.features.properties;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootFeaturesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
