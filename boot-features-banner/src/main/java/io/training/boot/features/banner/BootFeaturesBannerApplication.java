52ff32dae8d0ef2f0e290ecfd72776f0a8de4cd3
Percy0601 <2008dreams@2008.sina.com>
2016-09-02 19:14:37 +0800

    自定义banner

---
+++ boot-features-banner/src/main/java/io/training/boot/features/banner/BootFeaturesBannerApplication.java
@@ -0,0 +1,17 @@
+package io.training.boot.features.banner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootFeaturesBannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication application = new SpringApplication(BootFeaturesBannerApplication.class);
+		// 开启或者关闭Banner
+		// 当然这里也可以手动设置
+		// application.setBannerMode(Mode.OFF);
+		application.run(args);
+	}
+
+}
