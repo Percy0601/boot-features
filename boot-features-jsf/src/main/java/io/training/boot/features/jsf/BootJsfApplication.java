fa5245321fdc52c7d9d28cb8110e90e32538cb1e
Percy0601 <2008dreams@2008.sina.com>
2016-09-10 16:13:54 +0800

    整合jsf

---
+++ boot-features-jsf/src/main/java/io/training/boot/features/jsf/BootJsfApplication.java
@@ -0,0 +1,14 @@
+package io.training.boot.features.jsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJsfApplication {
+
+	public static void main(String[] args) throws Exception {
+		
+		SpringApplication.run(BootJsfApplication.class, args);
+	}
+
+}
---
+++ boot-features-jsf/src/main/java/io/training/boot/features/jsf/config/JsfConfig.java
@@ -0,0 +1,10 @@
+package io.training.boot.features.jsf.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:META-INF/spring/jsf-provider.xml")
+public class JsfConfig {
+
+}
