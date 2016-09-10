8f2877ce59be7d5387110a01d9966ecb18fb67b8
Percy0601 <2008dreams@2008.sina.com>
2016-09-10 16:34:23 +0800

    配置并初始化项目Test, Condition

---
+++ boot-features-condition/src/main/java/io/training/boot/features/condition/BootConditionApplication.java
@@ -0,0 +1,13 @@
+package io.training.boot.features.condition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootConditionApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(BootConditionApplication.class, args);
+	}
+
+}
---
+++ boot-features-test/src/main/java/io/training/boot/features/test/BootTestApplication.java
@@ -0,0 +1,13 @@
+package io.training.boot.features.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootTestApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(BootTestApplication.class, args);
+	}
+
+}
