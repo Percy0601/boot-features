2166241d7ab864dabe4daa25b4c1e1ad0dff0ce4
Percy0601 <2008dreams@2008.sina.com>
2016-09-20 10:26:18 +0800

    finish jersey

---
+++ boot-features-jersey/src/main/java/io/training/boot/features/jersey/BootJerseyApplication.java
@@ -0,0 +1,14 @@
+package io.training.boot.features.jersey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJerseyApplication {
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(BootJerseyApplication.class, args);
+	}
+
+
+}
