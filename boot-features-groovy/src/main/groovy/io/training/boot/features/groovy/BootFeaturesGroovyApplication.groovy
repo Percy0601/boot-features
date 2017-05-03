f5d099a4fe8eb93b3612eb829854bd6422bfad6f
Percy0601 <2008dreams@2008.sina.com>
2017-05-03 15:09:21 +0800

    增加groovy特性的支持

---
+++ boot-features-groovy/src/main/groovy/io/training/boot/features/groovy/BootFeaturesGroovyApplication.groovy
@@ -0,0 +1,12 @@
+package io.training.boot.features.groovy
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BootFeaturesGroovyApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run BootFeaturesGroovyApplication, args
+	}
+}
