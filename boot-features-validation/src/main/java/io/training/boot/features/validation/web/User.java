ba7624759634d30fd3660f6f2d3e9c97dd228bb0
Percy0601 <2008dreams@2008.sina.com>
2017-12-10 21:09:00 +0800

    使用分组方式，使得同一个类不同的验证方式

--- boot-features-validation/src/main/java/io/training/boot/features/validation/web/User.java
+++ boot-features-validation/src/main/java/io/training/boot/features/validation/web/User.java
@@ -3,11 +3,9 @@ package io.training.boot.features.validation.web;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
 @Data
 public class User {
-
     @NotNull(message = "主键不能为空", groups = UpdateGroup.class)
     private Long id;
     @NotNull(message = "姓名不能为空", groups = CreateGroup.class)
