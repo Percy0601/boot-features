a09be3049be2d7dad8a4da8ae9184f2d29309be4
Percy0601 <2008dreams@2008.sina.com>
2017-12-11 14:01:02 +0800

    如何捕获异常

--- boot-features-validation/src/main/java/io/training/boot/features/validation/web/ValidationServiceController.java
+++ boot-features-validation/src/main/java/io/training/boot/features/validation/web/ValidationServiceController.java
@@ -2,8 +2,13 @@ package io.training.boot.features.validation.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.ConstraintViolationException;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -15,17 +20,27 @@ public class ValidationServiceController {
     public String validCreateService() {
         User user = new User();
 
-        user.setAge(10);
-        user.setName("percy");
+//        user.setAge(10);
+//        user.setName("percy");
 
-        validationService.create(user);
+        try {
+            validationService.create(user);
+        } catch (ConstraintViolationException cve) {
+            cve.getConstraintViolations().forEach(it -> {
+                String message = it.getMessage();
+                log.error("当前错误信息为：{}", message);
+            });
+            log.error("调用服务验证异常：{}, 栈信息：", cve.getMessage(), cve);
+        } catch (Exception e) {
+            log.error("调用服务异常：{}, 栈信息：", e.getMessage(), e);
+        }
+
         return "验证服务的创建方式！";
     }
 
     @GetMapping("/valid-service-basic")
     public String validBasicService() {
-
-        validationService.basic("=============");
+        validationService.basic(null);
         return "验证服务的创建方式！";
     }
 
