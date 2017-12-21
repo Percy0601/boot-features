73093ce64d168ede83bda7f2a5f45426898095f6
Percy0601 <2008dreams@2008.sina.com>
2017-12-21 17:13:36 +0800

    调整配置

--- boot-features-jackson/src/main/java/io/training/boot/features/jackson/RespData.java
+++ boot-features-jackson/src/main/java/io/training/boot/features/jackson/RespData.java
@@ -3,9 +3,11 @@ package io.training.boot.features.jackson;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
 
+import javax.validation.constraints.Null;
+
 @Data
 public class RespData<T> {
     private String code;
-    @JsonView(View.Basic.class)
+    @JsonView(Object.class)
     private T result;
 }
