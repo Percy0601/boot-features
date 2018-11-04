549e656e48b60cda873675c8ddf8b319aaa388a4
Percy0601 <2008dreams@2008.sina.com>
2018-11-04 18:41:19 +0800

    spock with spring

---
+++ boot-features-test/src/test/java/io/training/boot/features/test/spock/SomeServiceSpec.groovy
@@ -0,0 +1,37 @@
+package io.training.boot.features.test.spock
+
+import io.training.boot.features.test.BootTestApplication
+import io.training.boot.features.test.SomeService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+
+@ContextConfiguration(classes = BootTestApplication)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
+class SomeServiceSpec extends Specification{
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    SomeService someService;
+
+
+
+    def "test autowired"() {
+        when:
+        String result = someService.handle("123");
+        log.info("==============={}", result);
+        then:
+        someService != null;
+
+    }
+
+
+
+
+
+}
