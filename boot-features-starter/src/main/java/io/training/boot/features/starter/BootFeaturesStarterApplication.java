044f0c2152a6fe9d02440109f1b74f0966d5a819
Percy0601 <2008dreams@2008.sina.com>
2018-12-10 21:59:46 +0800

    自定义starter

---
+++ boot-features-starter/src/main/java/io/training/boot/features/starter/BootFeaturesStarterApplication.java
@@ -0,0 +1,12 @@
+package io.training.boot.features.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootFeaturesStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootFeaturesStarterApplication.class, args);
+    }
+}
